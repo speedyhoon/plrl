4ed72e40f2da0888d05c8ef5ddb3276fef1c47e4
sh <16303791+speedyhoon@users.noreply.github.com>
2018-03-19 12:50:27 +1100

    Add Plural function.

---
+++ plural.go
@@ -0,0 +1,15 @@
+package utl
+
+// Plural returns an "s" if length != 1
+func Plural(length int, single, multiple string) string {
+	if length != 1 {
+		if multiple != "" {
+			return multiple
+		}
+		return "s"
+	}
+	if single != "" {
+		return single
+	}
+	return ""
+}
